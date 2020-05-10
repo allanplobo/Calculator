df249c924f6cc62fb33f8f35e86ed54ece17eea9
allanLobo <allan@prosystemnet.com.br>
2020-05-10 13:50:55 -0300

    Did some changes (general functions)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,15 @@ export class AppComponent {
 
   opActive: boolean = false;
   operation: string = "";
-
+  btSelect: string = "";
   texts: any[] = [];
 
 
   // Some Helper's Functions
 
   // This one, changes the color of Operation's buttons!
-  toggleColor() {
-    var x = document.getElementById("plusButton");
-    var currentColor = window.getComputedStyle(x, null).backgroundColor;
+  toggleColor(id: string) {
+    var x = document.getElementById(id);
 
     if (this.opActive == true) {
       x.style.color = "#e0ae09";
@@ -33,41 +32,49 @@ export class AppComponent {
     }
   }
 
+  doOperation(n1: string, n2: string, o1: string) {
+    var array = [this.mainNumberTemp, this.operation, this.mainNumber];
+    var result = eval(array[0] + array[1] + array[2]);
+    var textResult = this.texts.push("The result of " + this.mainNumberTemp + this.operation + this.mainNumber + " = " + result);
+
+    return {result, textResult};
+  }
+
 
   // Operation's buttons
 
 
-  btAc() {
-
-    if (this.mainNumber != "") {
-      this.texts.push("Cleaned! Last Result = " + this.mainNumber);
-      this.mainNumber = "";
-      this.mainNumberTemp = "";
-      this.placeholderNumber = "0";
-      var x = document.getElementById("plusButton");
-      this.opActive = false;
-      this.toggleColor();
-    }
-    else {
-      return;
-    };
-  }
-
-  mathOp(operation) {
+  mathOp(operation, id: string) {
     if (this.mainNumber !== "") {
       this.mainNumberTemp = this.mainNumber;
       this.placeholderNumber = this.mainNumber;
       this.opActive = true;
       this.operation = operation;
-      this.toggleColor();
+      this.btSelect = id;
+      this.toggleColor(id);
       this.mainNumber = "";
+    }
+    else if (this.mainNumber !== "" && this.opActive == true) {
+      this.toggleColor(id);
+    };
+  }
+
+  btAc() {
+
+    if (this.mainNumber !== "" || this.opActive == true) {
+      this.texts.push("Cleaned! Last Result = " + this.mainNumber);
+      this.mainNumber = "";
+      this.mainNumberTemp = "";
+      this.placeholderNumber = "0";
+      var x = document.getElementById(this.btSelect);
+      this.opActive = false;
+      this.toggleColor(this.btSelect);
     }
     else {
       return;
     };
-
-
   }
+
 
   btEquals() {
     if (this.mainNumberTemp == "") {
@@ -75,11 +82,9 @@ export class AppComponent {
     }
     else {
       this.opActive = false;
-      this.toggleColor();
-      var array = [this.mainNumberTemp, this.operation, this.mainNumber];
-      var result = eval(array[0] + array[1] + array[2]);
-      this.texts.push("The result of " + this.mainNumberTemp + this.operation + this.mainNumber + " = " + result);
-      this.mainNumber = result;
+      this.toggleColor(this.btSelect);
+      var x = this.doOperation(this.mainNumber, this.mainNumberTemp, this.operation);
+      this.mainNumber = x.result;
     }
 
   }
